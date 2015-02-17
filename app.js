a54835e528bde7de1dae3175313fb0cd52eaa44d
Stephen Richardson <sbr464@gmail.com>
2015-02-17 13:04:08 -0700

    updates

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 
+var trip = require('./public/voyage.js')
+
 var app = express();
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
@@ -8,6 +10,30 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
 	res.render('index');
 });
+
+app.get('/place/:location', function(req, res){
+
+  function search(nameKey, myArray){
+    for (var i=0; i < myArray.length; i++) {
+        if (myArray[i].name === nameKey) {
+            return myArray[i];
+        }
+    }
+}
+
+  
+  // If the :location exists in the trip database ...
+  var resultObject = search(req.params.location, trip);
+  console.log(resultObject);
+
+  res.render('index',
+    { locationName : resultObject.name,
+      locationDescrip : resultObject.description,
+      nextPort : resultObject.nextPort
+  })
+  // Look it up in the data file
+  // Inject the data??
+})
 
 var server = app.listen(5213, function() {
 	console.log('Express server listening on port ' + server.address().port);
---
+++ public/voyage.js
@@ -0,0 +1,41 @@
+var trip = [
+{
+  name: "Seville",
+  description: "Seville was founded as the Roman city of Hispalis, and was known",
+  nextPort : "Canary Islands"
+}, 
+
+{
+  name: "Canary Islands",
+  description: "test description canary",
+  nextPort : "Cape Verde"
+},
+
+{
+  name: "Cape Verde",
+  description: "test description cape verde",
+  nextPort : "Strait of Magellan"
+},
+
+{
+  name: "Strait of Magellan",
+  description: "test description SoM",
+  nextPort : "Guam"
+},
+
+{
+  name: "Guam",
+  description: "test description guam",
+  nextPort : "Philippines"
+},
+
+{
+  name: "Philippines",
+  description: "test description phil",
+  nextPort : ""
+}
+
+
+];
+
+module.exports = trip ;
