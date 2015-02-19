fc5f0767a28f34359e7c5832e21464aafe45d68f
Po Linn Chia <chiapolinn@gmail.com>
2015-02-19 12:10:15 -0700

    Improved voyage data

--- app.js
+++ app.js
@@ -7,24 +7,27 @@ app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
+
+// Helper search function
+function search(nameKey, myArray){
+  return _.find(myArray, function(obj){
+    return obj.name.toLowerCase() === nameKey.toLowerCase();
+  });
+}
+
+
+// Set starting 'index' as Seville
 app.get('/', function(req, res) {
 	res.redirect('/place/Seville');
 });
 
-  function search(nameKey, myArray){
 
-    return _.find(myArray, function(obj){
-      // console.log(obj.name, nameKey);
-      return obj.name.toLowerCase() === nameKey.toLowerCase();
-    });
-  }
 app.get('/place/:location', function(req, res){
 
-
   
-  // If the :location exists in the trip database ...
   var resultObject = search(req.params.location, trip);
 
+  // If the :location exists in the trip database ...
   if(resultObject){
     res.render('index',
       { locationName : resultObject.name,
@@ -32,14 +35,15 @@ app.get('/place/:location', function(req, res){
         nextPort : resultObject.nextPort,
         tripImage : resultObject.image
 
-    })    
+    });    
   }
   // Else send them to the 404
   else {
     res.render('404');
   }
-})
+});
 
+// Give users a JSON response on /next query
 app.get('/next', function(req, res){
   var currentLocation = req.query.location;
   var resultObject = search(currentLocation, trip);
@@ -48,8 +52,7 @@ app.get('/next', function(req, res){
   res.jsonp({ location: currentLocation, 
               nextLocation: nextLocation });
 
-})
-
+});
 
 
 var server = app.listen(5213, function() {
--- public/voyage.js
+++ public/voyage.js
@@ -1,38 +1,38 @@
 var trip = [
 {
   name: "Seville",
-  description: "Seville was founded as the Roman city of Hispalis, and was known",
+  description: "Seville was founded as the Roman city of Hispalis.",
   nextPort : "Canary Islands",
   image : "http://media-cdn.tripadvisor.com/media/photo-s/03/9b/30/2b/seville.jpg"
 }, 
 
 {
   name: "Canary Islands",
-  description: "test description canary",
+  description: "The Canary Islands, also known as the Canaries (Spanish: Canarias), are a Spanish archipelago located just off the southwest coast of mainland Morocco, 100 kilometres (62 miles) west of the southern border of Morocco.",
   nextPort : "Cape Verde"
 },
 
 {
   name: "Cape Verde",
-  description: "test description cape verde",
+  description: "Portuguese explorers discovered and colonized the uninhabited islands in the 15th century, the first European settlement in the tropics. Ideally located for the Atlantic slave trade, the islands grew prosperous and often attracted privateers and pirates, among them Sir Francis Drake in the 1580s.",
   nextPort : "Strait of Magellan"
 },
 
 {
   name: "Strait of Magellan",
-  description: "test description SoM",
+  description: "The Strait of Magellan, also called the Straits of Magellan, is a navigable sea route separating mainland South America to the north and Tierra del Fuego to the south.",
   nextPort : "Guam"
 },
 
 {
   name: "Guam",
-  description: "test description guam",
+  description: "When Magellan arrived on Guam, he was greeted by hundreds of small outrigger canoes that appeared to be flying over the water, due to their considerable speed. These outrigger canoes were called Proas, and resulted in Magellan naming Guam Islas de las Velas Latinas ('Islands of the Lateen sails').",
   nextPort : "Philippines"
 },
 
 {
   name: "Philippines",
-  description: "test description phil",
+  description: "The coming of Ferdinand Magellan in 1521 marked the beginning of Spanish colonization.",
   nextPort : ""
 }
 
